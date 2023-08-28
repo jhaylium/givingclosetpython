f7c07300cf3d00072acb886852db10d2ebac202b
Jeff <jmccain@haylium.com>
2023-08-28 17:31:50 -0400

    changes to correct for split to multiple forms

--- send-monthly-email.py
+++ send-monthly-email.py
@@ -1,23 +1,42 @@
 from app.GravityForms import GravityForms
 import os
+import re
 from app.email_ses import send_email
 from datetime import datetime, date, timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-def lambda_handler(event, context):
-    html_file = f"{os.environ['LAMBDA_TASK_ROOT']}/app/gcp-automated-process.html"
-    last_day_prev_mth = date.today().replace(day=1) - timedelta(days=1)
-    first_day_prev_mth = date.today().replace(day=1) - timedelta(days=last_day_prev_mth.day)
-    dates = [first_day_prev_mth, last_day_prev_mth]
-    id = 6
-    gf = GravityForms(secret=os.environ['grav_secret'], key=os.environ['grav_key'])
-    gf.write_form_entires_to_file(filename='/tmp/data.csv', form_id=id, date=dates)
-    #
-    text = "Monthly files are attached."
-    html = open(html_file).read()
-    sender = "info@givingclosetproject.org"
-    to =[
+last_day_prev_mth = date.today().replace(day=1) - timedelta(days=1)
+first_day_prev_mth = date.today().replace(day=1) - timedelta(days=last_day_prev_mth.day)
+dates = [first_day_prev_mth, last_day_prev_mth]
+
+
+areas_served = [16, 23]
+today = datetime.today().strftime('%Y-%m-%d')
+yrmth = f'_{last_day_prev_mth.strftime("%Y_%m")}'
+attachments = []
+for id in areas_served:
+    id
+    print(id, filepath)
+    gf = GravityForms(secret=os.environ['secret'], key=os.environ['key'])
+    form_info = gf.get_form_info(id)
+    form_name = re.sub('[^a-zA-Z0-9 \n\.]', '_', form_info['title']).replace(' ', '_')
+    form_name = form_name.replace('___', '_')
+    print(form_name)
+    filepath = f'tmp/{form_name}{yrmth}.csv'
+    print(filepath)
+    attachments.append(filepath)
+    gf.write_form_entires_to_file(filename=filepath, form_id=id,form=form_info, date=dates)
+
+text = "Monthly files are attached."
+html = open("files\\gcp-automated-process.html").read()
+#awshtml = f"{os.environ['LAMBDA_TASK_ROOT']}/app/gcp-automated-process.html"
+sender = "info@givingclosetproject.org"
+to = [
     ]
-    message = {"to": to, "from": sender, "subject": "GCP Monthly File",
-               "attachments": ["/tmp/Duval_County.csv", "/tmp/Palm_Beach_County.csv"], "text": text, "html": html}
-    send_email(message)
+
+message = {"to": to, "from": sender, "subject": "GCP Monthly File",
+           "attachments": attachments, "text": text, "html": html}
+send_email(message)
